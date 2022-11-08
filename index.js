87fee54982edc72382ce4641a470ea92bac2b396
fahad Mridha <sohelcse16@gmail.com>
2022-11-09 03:47:22 +0600

    services data fetch route setup

--- index.js
+++ index.js
@@ -13,17 +13,27 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ouw6pvz.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    const servicesCollection = client
+      .db("servicesReview")
+      .collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } catch (error) {}
+}
+run().catch((error) => console.error(error.bgRed));
 
 app.get("/", (req, res) => {
   res.send("review server is running");
